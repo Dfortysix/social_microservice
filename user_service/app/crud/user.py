34f88063345a7929af0d33e88fafd53ac513edd1
dfortysix <tridug04062003@gmail.com>
2025-06-04 04:23:34 +0000

    update[user_service]: add some functions in crud

---
+++ user_service/app/crud/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from app.db.models import User
+from app.schemas.user import UserCreate
+from app.services.auth import get_password_hash
+
+async def get_user_by_email(db: AsyncSession, email: str):
+    result = await db.execute(select(User).where(User.email == email))
+    return result.scalar_one_or_none()
+
+async def create_user(db: AsyncSession, user: UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    await db.commit()
+    await db.refresh(db_user)
+    return db_user
