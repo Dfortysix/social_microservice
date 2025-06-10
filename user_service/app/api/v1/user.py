2bd2945a6aee574ad95aa433e1a14800480f4e1d
dfortysix <tridug04062003@gmail.com>
2025-06-10 04:27:26 +0000

    update[api_v1]: add api get users

--- user_service/app/api/v1/user.py
+++ user_service/app/api/v1/user.py
@@ -4,15 +4,11 @@ from app.schemas.user import UserCreate, UserOut, Token
 from app.crud.user import get_user_by_email, create_user
 from app.db.session import AsyncSessionLocal
 from app.services.auth import verify_password, create_access_token
+from app.dependencies.auth import get_current_user
+from app.dependencies.db import get_db
+
 
 router = APIRouter(prefix="/users", tags=["Users"])
-
-def get_db():
-    db = AsyncSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 @router.post("/register", response_model=UserOut)
 async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
@@ -28,3 +24,7 @@ async def login(user: UserCreate, db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     access_token = create_access_token({"sub": db_user.email})
     return {"access_token": access_token, "token_type": "bearer"}
+
+@router.get("/me", response_model=UserOut)
+async def get_me(current_user: UserOut = Depends(get_current_user)):
+    return current_user
