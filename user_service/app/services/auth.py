decbb19b46999f0da3c8cc73ef84927d8a32b9ce
dfortysix <tridug04062003@gmail.com>
2025-06-10 04:26:49 +0000

    update[services]: update auth

--- user_service/app/services/auth.py
+++ user_service/app/services/auth.py
@@ -3,11 +3,20 @@ from jose import jwt, JWTError
 from datetime import datetime, timedelta
 import os
 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.session import AsyncSessionLocal
+from app.dependencies.auth import get_current_user
+
+
 SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
+
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
@@ -27,3 +36,4 @@ def decode_access_token(token: str):
         return payload.get("sub")
     except JWTError:
         return None
+
