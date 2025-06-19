1ef7868b3be639cc9c67c8532ea71e3650b85779
Nguyen Tri Dung <dungnt1@newwave.vn>
2025-06-19 10:44:16 +0700

    update[user_service_main]: add session middleware

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -3,6 +3,18 @@
 from fastapi import FastAPI
 from app.db.init_db import init_db
 from app.api.v1 import user
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from email.message import EmailMessage
+from starlette.middleware.sessions import SessionMiddleware
+
+# Load biến môi trường từ file .env
+env_path = Path("app/.env")
+load_dotenv(env_path)
+
+
+allowed_origins = [os.getenv("POST_SERVICE_URL"), os.getenv("USER_SERVICE_URL")]
 
 app = FastAPI(
     title="User Service",
@@ -14,14 +26,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# 🔒 Thêm SessionMiddleware (dùng cho OAuth)
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=os.getenv("SESSION_SECRET_KEY", "your-super-secret-key")
+)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://100.72.170.73:8001"],  # Swagger của post_service
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.on_event("startup")
 async def startup():
